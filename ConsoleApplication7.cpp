f1d9a02ea045019a90bca4ad5d03871e65be4e60
maziom <92737137+maziom@users.noreply.github.com>
2021-12-07 23:15:02 +0100

    Add files via upload

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	//zad1
+	//nie bedzie to interpretowane jako lancuch znakowy lecz, trzy oddzielne elementy
+	
+	//zad2
+	//nie poprawne pobranie elementow tablicy nalezy uzyc getline() oraz nie prawidlowa tablica nalezy uzyc char
+
+/*	//zad3
+
+	char tab[] = "wiosna";
+
+	cout << "Rozmiar tablicy: " << size(tab); */
+
+/*	//zad4
+
+	cout << "wpisz swoje imie: ";
+
+	char tab[50];
+
+	cin.get(tab, 50);
+
+	char tab1[100];
+
+	cout << "wpisz swoje nazwisko: ";
+
+	cin.get();
+
+	cin.get(tab1, 100);
+
+	strcat_s(tab1, tab);
+
+	cout << tab1;
+
+	char tab3[500];
+
+	strcpy_s(tab3, tab);
+
+	cout << tab3;
+
+	cout << "\n liczba znakow: " << strlen(tab3);
+
+	cout << "\n rozmiar tablicy: " << size(tab3);
+	*/
+
+	
+	
+
+}
